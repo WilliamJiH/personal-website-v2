ed51b8dd27c9c41b87fa89837583c4c47c3febf4
William ji <williamji12345@gmail.com>
2021-09-15 00:10:49 -0400

    initial commit

---
+++ src/data/workData.js
@@ -0,0 +1,19 @@
+import stockerrr from '../image/workImages/stockerrr.png';
+import portfolio from '../image/workImages/portfolio.jpeg';
+
+export const workData = [
+  {
+    name: 'stockerrr',
+    description: 'An US stock watchlist program with auto refresh',
+    code: 'https://github.com/WilliamJiH/stockerrr',
+    demo: 'https://stockerrr.herokuapp.com/',
+    image: stockerrr,
+  },
+  {
+    name: 'portfolio',
+    description: 'My personal website upgraded version',
+    code: '',
+    demo: '',
+    image: portfolio,
+  },
+];
